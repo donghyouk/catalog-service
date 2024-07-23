cb25a89acd574112e6cd69189c7f965d7ae5f112
dhchoi <donald.ember536@gmail.com>
2024-07-23 05:26:55 +0000

    p154 4.2

--- src/main/java/com/polarbookshop/catalog_service/HomeController.java
+++ src/main/java/com/polarbookshop/catalog_service/HomeController.java
@@ -3,11 +3,19 @@ package com.polarbookshop.catalog_service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.polarbookshop.catalog_service.config.PolarProperties;
+
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "도서 카탈로그에 오신 것을 환영합니다.";
+        return polarProperties.getGreeting();
     }
 }
